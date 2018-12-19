c0d54ad24fcf62a9e6e8a7106a90f1411040c425
Mario González Carpintero <gonzalezm@fanae127.geol.uniovi.es>
2018-12-19 15:27:58 +0100

    upgraded versions of batch.sh and submit.sh

---
+++ plotter/WZ/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+
+
+SOURCE="${BASH_SOURCE[0]}"
+# ./path/to/thisFile/thisFile.sh
+#echo $SOURCE
+
+RDIR="$( dirname "$SOURCE" )"
+# ./path/to/thisFile/ (la ruta a este script desde donde es llamado)
+#echo $RDIR
+
+DIR="$( cd -P "$RDIR" && pwd )"
+    # sigue la ruta RDIR y haz un pwd cuando llegues.
+    # -P se asegura de que bash está siguiendo un camino real, no un link
+    # simbolico.
+echo "Hello from $DIR! I'm the bash script you just ran :D"
+
+# Running with -q proof is only different in that it has a small time limit
+# (~2h). -q batch has a 2 days limit.
+qsub -q batch -l nodes=gae013.ciencias.uniovi.es:ppn=32 -F $DIR "$DIR"/send.sh
+echo "ruuuning!"
+
